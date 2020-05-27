18d09a8060cf8ffb80a50c9bcd2b2507e54e7be5
Maksim_Naumovich <Maksim_Naumovich@epam.com>
2020-05-27 11:05:50 +0300

    sample

---
+++ src/main/java/by/pvt/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package by.pvt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json;charset=UTF-8");
+		PrintWriter writer = resp.getWriter();
+		writer.write("{ \"name1\":\"value1\", \"name2\":\"value2\"}");
+	}
+	
+}
